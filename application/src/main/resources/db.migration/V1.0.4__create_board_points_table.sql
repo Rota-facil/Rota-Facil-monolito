2ad31b0a845d0de5c80d5ea6f7eb3d701c01d403
Thauã Gabriel <tagashi.dev@gmail.com>
2026-09-12 13:57:15 -0300

    infra: add board points table

---
+++ application/src/main/resources/db.migration/V1.0.4__create_board_points_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS board_points_tb (
+    board_point_id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL,
+    longitude DOUBLE PRECISION NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
