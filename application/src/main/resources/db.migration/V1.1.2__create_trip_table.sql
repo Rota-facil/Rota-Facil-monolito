3853b6a25efec039800f3d293f5d319ca6ade906
Thauã Gabriel <tagashi.dev@gmail.com>
2026-09-13 11:56:10 -0300

    fix: adjusting migration

--- application/src/main/resources/db.migration/V1.1.2__create_trip_table.sql
+++ application/src/main/resources/db.migration/V1.1.2__create_trip_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS trips_tb (
     trip_id UUID PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    bus_id UUID NOT NULL,
+    vehicle_id UUID NOT NULL,
     route_id UUID NOT NULL,
     latitude DOUBLE PRECISION NOT NULL DEFAULT 0.0,
     longitude DOUBLE PRECISION NOT NULL DEFAULT 0.0,
@@ -11,6 +11,6 @@ CREATE TABLE IF NOT EXISTS trips_tb (
     reason_of_cancellation TEXT,
     created_at DATE NOT NULL DEFAULT CURRENT_DATE,
 
-    CONSTRAINT fk_trips_bus FOREIGN KEY (bus_id) REFERENCES bus_tb(bus_id),
+    CONSTRAINT fk_trips_vehicles FOREIGN KEY (vehicle_id) REFERENCES vehicles_tb(vehicle_id),
     CONSTRAINT fk_trips_routes FOREIGN KEY (route_id) REFERENCES routes_tb(route_id)
     );
