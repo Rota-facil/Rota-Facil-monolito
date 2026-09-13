eb99c9331f89fd1ea26e0d98ee3723dd800cf21c
Thauã Gabriel <tagashi.dev@gmail.com>
2026-09-13 11:46:03 -0300

    infra: add trip table

---
+++ application/src/main/resources/db.migration/V1.1.2__create_trip_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS trips_tb (
+    trip_id UUID PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    bus_id UUID NOT NULL,
+    route_id UUID NOT NULL,
+    latitude DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    longitude DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    prefecture_id UUID NOT NULL,
+    students BIGINT NOT NULL DEFAULT 0,
+    actual_status TEXT NOT NULL,
+    reason_of_cancellation TEXT,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+
+    CONSTRAINT fk_trips_bus FOREIGN KEY (bus_id) REFERENCES bus_tb(bus_id),
+    CONSTRAINT fk_trips_routes FOREIGN KEY (route_id) REFERENCES routes_tb(route_id)
+    );
