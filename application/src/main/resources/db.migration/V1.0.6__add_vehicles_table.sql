fbb5415cc14e5b63b6202ad4aa44193aedb01336
Thauã Gabriel <tagashi.dev@gmail.com>
2026-09-12 23:05:40 -0300

    infra: add vehicle table

--- application/src/main/resources/db.migration/V1.0.6__add_vehicles_table.sql
+++ application/src/main/resources/db.migration/V1.0.6__add_vehicles_table.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS bus_tb (
-    bus_id UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS vehicles_tb (
+    vehicle_id UUID PRIMARY KEY,
     driver_id UUID,
     prefecture_id UUID NOT NULL,
     capacity BIGINT NOT NULL,
@@ -8,5 +8,5 @@ CREATE TABLE IF NOT EXISTS bus_tb (
     status VARCHAR(30) NOT NULL DEFAULT 'OUT_OF_OPERATION',
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT bus_users FOREIGN KEY (driver_id) REFERENCES users_tb(user_id)
+    CONSTRAINT vehicle_users FOREIGN KEY (driver_id) REFERENCES users_tb(user_id)
 );
